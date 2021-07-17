25b925c1c3e1e8f967ff336cba1cbccab6c46b03
Junior Souza <junior.gs49@gmail.com>
2021-07-17 20:07:40 -0300

    Projeto Kit Quebrada

---
+++ src/components/ixepa.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+
+ <label for="username">Username:</label>
+    <input type="text" id="username" name="username">
+    <div> 
+
+    </div>
+    <label for="pass">Password:</label>
+    <input type="password" id="pass" name="password"
+           minlength="8" required>
+   <div>
+           <button>Login</button>
+   </div>
+ 
+
+</div>
+</template>
+
+<script>
+export default {    
+}
+</script>
+
+<style scoped>
+
+
+</style>
